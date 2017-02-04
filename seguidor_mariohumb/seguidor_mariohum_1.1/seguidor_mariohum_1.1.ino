1289f732c289879b3a954dbfc5ce2b5e646a7ea7
Gerardo Díaz <gerardo@MacBook-Pro-de-Gerardo.local>
2017-02-04 12:21:21 -0600

    Espacio en líneas

--- seguidor_mariohumb/seguidor_mariohum_1.1/seguidor_mariohum_1.1.ino
+++ seguidor_mariohumb/seguidor_mariohum_1.1/seguidor_mariohum_1.1.ino
@@ -1,7 +1,7 @@
 #include <AFMotor.h>
 AF_DCMotor motor_derecho(4);   //motor derecho
 AF_DCMotor motor_izquierdo(3);   //motor izquierdo
-// TCS230 color recognition sensor 
+// TCS230 color recognition sensor
 // Sensor connection pins to Arduino are shown in comments
 /*
 Color Sensor derecho  Arduino
@@ -19,8 +19,8 @@ Color Sensor izquierdo Arduino
  VCC               5V
  GND               GND
  s0                52 verde
- s1                50 azul    
- s2                48 azul/blanco 
+ s1                50 azul
+ s2                48 azul/blanco
  s3                46 verde/blanco
  OUT               44 cafe/blanco
 */
@@ -48,7 +48,7 @@ int l5=1;
 int l6=1;
 int l7=1;
 int l8=1;
-//colores 
+//colores
 int neg=0;
 int bco=1;
 
@@ -69,42 +69,42 @@ void setup() {
    pinMode(der_4,INPUT);
    //s5
    pinMode(der_5,INPUT);
- 
+
 }
 
 
   void loop() {
-   
+
    l5=digitalRead(izq_1);
    l4=digitalRead(izq_2);
    l3=digitalRead(centro_3);
    l2=digitalRead(der_4);
    l1=digitalRead(der_5);
-   
 
 
-   
-   
+
+
+
    if(l5==neg ){
-        motor_derecho.setSpeed(250);
+       motor_derecho.setSpeed(250);
        motor_derecho.run(BACKWARD);
        motor_izquierdo.setSpeed(200);
        motor_izquierdo.run(FORWARD);
   }
    if(l1==neg ){
-        motor_derecho.setSpeed(175);
+       motor_derecho.setSpeed(175);
        motor_derecho.run(FORWARD);
        motor_izquierdo.setSpeed(250);
        motor_izquierdo.run(BACKWARD);
   }
 
-if((l1==bco && l2==bco && l4==bco && l5==bco)){ 
+if((l1==bco && l2==bco && l4==bco && l5==bco)){
        motor_derecho.setSpeed(100);
        motor_derecho.run(FORWARD);
        motor_izquierdo.setSpeed(100);
        motor_izquierdo.run(FORWARD);
 }
-   
+
 if (l2==neg && l5==bco && l1==bco ){
        motor_derecho.setSpeed(100);
        motor_derecho.run(FORWARD);
@@ -112,26 +112,19 @@ if (l2==neg && l5==bco && l1==bco ){
        motor_izquierdo.run(BACKWARD);
 }
 
- 
-  
-  
+
+
+
 
 if (l4==neg && l1==bco && l5==bco ){
        motor_derecho.setSpeed(20);
        motor_derecho.run(BACKWARD);
        motor_izquierdo.setSpeed(150);
-  
-       
+
+
        motor_izquierdo.run(FORWARD);
 }
 
 
 
 }
-
-
- 
-
-               
-
-
